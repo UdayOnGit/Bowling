8264053abe0c9aefbb0219d69b0504a184a8f93b
Uday Jadhav <uday.k.jadhav@gmail.com>
2021-06-14 19:43:32 +1000

    Add logic to calculate score per frame and end result Add unit tests to verify the logic

--- src/Services/BowlingService.cs
+++ src/Services/BowlingService.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using Bowling.Models;
 using Microsoft.Extensions.Logging;
 
@@ -6,6 +8,8 @@ namespace Bowling.Services
     public class BowlingService : IBowlingService
     {
         private readonly ILogger<BowlingService> _logger;
+        private const int MaximumFrameCount = 10;
+        private const int StrikeOrSparePoints = 10;
 
         public BowlingService(ILogger<BowlingService> logger)
         {
@@ -16,24 +20,83 @@ namespace Bowling.Services
         {
             _logger.LogInformation($"{string.Join(",", requestModel.PinsDowned)}");
 
-            var result = new ResponseModel
+            var pins = requestModel.PinsDowned.ToList();
+
+            var frames = new List<Frame>();
+            int rollNumber = 0;
+            var pinsDowned = requestModel.PinsDowned;
+
+            for (int frameNumber = 0; frameNumber < MaximumFrameCount; frameNumber++)
             {
-                GameCompleted = true,
-                FrameProgressScores = new string[]
+                if (rollNumber >= pinsDowned.Length)
                 {
-                    "9", 
-                    "17",
-                    "26",
-                    "35", 
-                    "44", 
-                    "44", 
-                    "52", 
-                    "61", 
-                    "69", 
-                    "76"
+                    break;
                 }
+
+                int previousFrameScore = frames.LastOrDefault()?.Score ?? 0;
+                var frame = new Frame();
+                frames.Add(frame);
+                if (IsItAStrike(pinsDowned, rollNumber))
+                {
+                    frame.Score = previousFrameScore + StrikeOrSparePoints + GetStrikeBonus(pinsDowned, rollNumber);
+                    rollNumber++;
+                }
+                else if (IsItASpare(pinsDowned, rollNumber))
+                {
+                    frame.Score = previousFrameScore + StrikeOrSparePoints + GetSpareBonus(pinsDowned, rollNumber);
+                    rollNumber += 2;
+                }
+                else
+                {
+                    frame.Score = previousFrameScore + pinsDowned[rollNumber] + pinsDowned[rollNumber + 1];
+                    rollNumber += 2;
+                }
+            }
+
+            return GetResponseModel(frames);
+        }
+
+        private ResponseModel GetResponseModel(List<Frame> frames)
+        {
+            var frameScores = frames.Select(x => x.Score.ToString());
+
+            return new ResponseModel
+            {
+                GameCompleted = frames.Count == MaximumFrameCount,
+                FrameProgressScores = frameScores.ToArray()
             };
-            return result;
+        }
+
+        private bool IsItAStrike(int[] pinsDowned, int rollNumer) => pinsDowned[rollNumer] == 10;
+
+        private int GetStrikeBonus(int[] pinsDowned, int rollNumber)
+        {
+            var strikeScore = 0;
+            if ((rollNumber + 1) < pinsDowned.Length)
+            {
+                strikeScore += pinsDowned[rollNumber + 1];
+            }
+            if ((rollNumber + 2) < pinsDowned.Length)
+            {
+                strikeScore += pinsDowned[rollNumber + 2];
+            }
+            return strikeScore;
+        }
+
+        private bool IsItASpare(int[] pinsDowned, int rollNumber) => pinsDowned[rollNumber] + pinsDowned[rollNumber + 1] == 10;
+
+        private int GetSpareBonus(int[] pinsDowned, int rollNumber)
+        {
+            if ((rollNumber + 2) < pinsDowned.Length)
+                return pinsDowned[rollNumber + 2];
+            else
+                return 0;
+        }
+
+        public class Frame
+        {
+            public int Score { get; set; }
         }
     }
+
 }
--- tests/BowlingTests/Services/BowlingServiceTests.cs
+++ tests/BowlingTests/Services/BowlingServiceTests.cs
@@ -9,8 +9,105 @@ namespace BowlingTests.Services
 {
     public class BowlingServiceTests
     {
+        private Mock<ILogger<BowlingService>> _logger;
+        private BowlingService _service;
+
+        private void Setup()
+        {
+            _logger = new Mock<ILogger<BowlingService>>();
+            _service = new BowlingService(_logger.Object);
+        }
+
         [Fact]
-        public void Should_Return_Frame_Progress_And_Game_Result()
+        public void Test_All_Strikes()
+        {
+            // Arrange
+            Setup();
+            var requestModel = new RequestModel
+            {
+                PinsDowned = new int[] { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 }
+            };
+            var responseModel = new ResponseModel
+            {
+                FrameProgressScores = new string[] { "30", "60", "90", "120", "150", "180", "210", "240", "270", "300" },
+                GameCompleted = true
+            };
+
+            // Act
+            var result = _service.GetGameResult(requestModel);
+
+            // Assert
+            result.Should().BeEquivalentTo<ResponseModel>(responseModel);
+        }
+
+        [Fact]
+        public void Test_All_Spares()
+        {
+            // Arrange
+            Setup();
+            var requestModel = new RequestModel
+            {
+                PinsDowned = new int[] { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 }
+            };
+            var responseModel = new ResponseModel
+            {
+                FrameProgressScores = new string[] { "15", "30", "45", "60", "75", "90", "105", "120", "135", "150" },
+                GameCompleted = true
+            };
+
+            // Act
+            var result = _service.GetGameResult(requestModel);
+
+            // Assert
+            result.Should().BeEquivalentTo<ResponseModel>(responseModel);
+        }
+
+        [Fact]
+        public void Test_All_Gutter()
+        {
+            // Arrange
+            Setup();
+            var requestModel = new RequestModel
+            {
+                PinsDowned = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+            };
+            var responseModel = new ResponseModel
+            {
+                FrameProgressScores = new string[] { "0", "0", "0", "0", "0", "0", "0", "0", "0", "0" },
+                GameCompleted = true
+            };
+
+            // Act
+            var result = _service.GetGameResult(requestModel);
+
+            // Assert
+            result.Should().BeEquivalentTo<ResponseModel>(responseModel);
+        }
+
+        [Fact]
+        public void Test_All_With_1_Pin()
+        {
+            // Arrange
+            Setup();
+            var requestModel = new RequestModel
+            {
+                PinsDowned = new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
+            };
+            var responseModel = new ResponseModel
+            {
+                FrameProgressScores = new string[] { "2", "4", "6", "8", "10", "12", "14", "16", "18", "20" },
+                GameCompleted = true
+            };
+
+            // Act
+            var result = _service.GetGameResult(requestModel);
+
+            // Assert
+            result.Should().BeEquivalentTo<ResponseModel>(responseModel);
+        }
+
+        [Fact]
+        public void Should_Return_Correct_Frame_Progress_And_Game_Result_For_Random_Rolls()
         {
             // Arrange
             var logger = new Mock<ILogger<BowlingService>>();
@@ -31,5 +128,28 @@ namespace BowlingTests.Services
             // Assert
             result.Should().BeEquivalentTo<ResponseModel>(responseModel);
         }
+
+        [Fact]
+        public void Test_Random_Pins_Incomplete()
+        {
+            // Arrange
+            Setup();
+            var requestModel = new RequestModel
+            {
+                PinsDowned = new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
+            };
+            var responseModel = new ResponseModel
+            {
+                FrameProgressScores = new string[] { "2", "4", "6", "8", "10", "12", "14", "16" },
+                GameCompleted = false
+            };
+
+            // Act
+            var result = _service.GetGameResult(requestModel);
+
+            // Assert
+            result.Should().BeEquivalentTo<ResponseModel>(responseModel);
+        }
+
     }
 }
